d2e8fa310c66385a2e55aa14aeecc30ec702be34
KmolYuan <pyslvs@gmail.com>
2021-09-24 13:51:45 +0800

    Improve the length input.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 extern "C" {
-    fn test_(a: *mut f64, n: *const i32);
+    fn test_(a: *mut f64, n: &i32);
 }
 
 fn main() {
     let mut a = [0.5, 20.25];
-    unsafe { test_(a.as_mut_ptr(), &2) };
+    unsafe { test_(a.as_mut_ptr(), &(a.len() as i32)) };
     println!("Rust: {}, {}", a[0], a[1]);
 }
